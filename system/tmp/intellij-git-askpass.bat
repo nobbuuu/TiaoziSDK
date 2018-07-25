be8385df2bc803117a18fd1c4d223e7659640742
tiaozi <1184824553@qq.com>
2018-07-25 22:26:22 +0800

    Tiaozi

---
+++ system/tmp/intellij-git-askpass.bat
@@ -0,0 +1,2 @@
+@echo off
+"F:/AS3.1/jre/jre/bin/java" -cp "F:/AS3.1/plugins/git4idea/lib/git4idea-rt.jar;F:/AS3.1/lib/xmlrpc-2.0.jar;F:/AS3.1/lib/commons-codec-1.9.jar;F:/AS3.1/lib/util.jar" org.jetbrains.git4idea.http.GitAskPassApp %*
---
+++ system/tmp/intellij-git-ssh.bat
@@ -0,0 +1,2 @@
+@echo off
+"F:/AS3.1/jre/jre/bin/java" -cp "F:/AS3.1/plugins/git4idea/lib/git4idea-rt.jar;F:/AS3.1/lib/xmlrpc-2.0.jar;F:/AS3.1/lib/commons-codec-1.9.jar;F:/AS3.1/lib/util.jar;F:/AS3.1/lib/trilead-ssh2-build-217-jenkins-11.jar;F:/AS3.1/lib/eddsa-0.2.0.jar;F:/AS3.1/lib/jbcrypt-1.0.0.jar" org.jetbrains.git4idea.ssh.SSHMain %*
